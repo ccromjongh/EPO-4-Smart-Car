851257c9585edc152c93752a729dd7481f953e5d
Casper Cromjongh <casper.cromjongh@hotmail.com>
2017-05-11 08:58:45 +0200

    Commits from Niels which he couldn't push. Fixes for when starting from a long distance when the sensors can't see the wall. Initialise structs before we start driving.

--- DelayMeasurement.m
+++ DelayMeasurement.m
@@ -1,7 +1,6 @@
-clear variables;
 %% Script settings
-comport = '\\.\COM7';       % Name of the port to be opened
-re_open_port = true;       % Close and open port
+comport = '\\.\COM9';       % Name of the port to be opened
+re_open_port = false;       % Close and open port
 
 % Create instance of control class
 KITT = testClass;
@@ -19,9 +18,9 @@ KITT.getDistance();
 leftDistance = KITT.leftDistance;   % Initial left sensor distance
 rightDistance = KITT.rightDistance; % Initial right sensor distance
 request = 0;                        % Amount of requests send to KITT
+KITT.setSteerDirection(4);
 tic
 KITT.setMotorSpeed(30);
-KITT.setSteerDirection(4);
 while (true)
     KITT.getDistance();
     NewleftDistance = KITT.leftDistance;
@@ -33,5 +32,5 @@ while (true)
         request = request + 1;
     end
 end
-
-KITT.setMotorSpeed(0);
+delayarray(end+1) = delay_time;
+KITT.setMotorSpeed(15);
