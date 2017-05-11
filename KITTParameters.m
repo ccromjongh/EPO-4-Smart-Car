1764ff3adda2abcfbad02f35f8723951f021c858
koenmesman <koenmesman@gmail.com>
2017-05-11 08:53:31 +0200

    Add files via upload

---
+++ KITTParameters.m
@@ -0,0 +1,6 @@
+m =13;      %mass kg
+b = 0.0015; %friction coefficient 1
+c = 0.02;   %drag coefficient 2
+Fa = 80;    %max acceleration     max =80
+Fb = -120;  %max braking          max =-120
+Vini = 0;   %initial velocity
