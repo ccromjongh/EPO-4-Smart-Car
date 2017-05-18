38ffede17be11674f783dd2f23aac3e2d726b9ec
Casper Cromjongh <casper.cromjongh@hotmail.com>
2017-05-18 09:54:28 +0200

    Cleaned up the repo, downloaded new EPOCommunications, PA_record, playrec and the MDS_rotate script.

--- calcVirtualPos.m
+++
@@ -1,5 +0,0 @@
-function obj = calcVirtualPos (obj, time)
-    obj.t = time;                               % Set current time
-    obj.dt = double(obj.t - obj.tOld)*10^(-6);	% Calculate dt
-    obj.dVirt = obj.d + obj.v * obj.dt;         % Calculae virtual distance (speed * dt)
-end
