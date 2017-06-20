3d6be10cac6415b84a3dd66adc8fc1c490e8cecb
NielsFakkel <n.e.fakkel@student.tudelft.nl>
2017-06-20 10:50:29 +0200

    Included time model

--- ControlKitt.m
+++ ControlKitt.m
@@ -1,46 +1,79 @@
-% Load playfield data
+
 JSON = fileread('field_K.json');
 field_data = jsondecode(JSON);
 clear JSON;
 
+% Open port
+KITT = testClass;
+% KITT.openPort('//./COM12');
+
+% Setup the beacon
+load audiodata_96k.mat;
+KITT.setupBeacon(Timer0, Timer1, Timer3, code);
+
 r1 = 0.1;
+p = gcp();
 
 % Set start and end location
 start_location = [-2 -2];
-start_angle = 0;
+start_angle = pi/2;
 final_location = [2 2];
+
+% Plot the field
+distance = zeros(1,5);
+mic = 1:5;
+playfield_plot(distance, mic, start_location(1), start_location(2), field_data);
 
 location = start_location;
 
 while true
-    % Record audio from KITT's beacon and find the propagation delay
-    [Hdist, Fs] = RecordLive(true, 5);
-    
-    % Execute TDOA script to find KITT's location
-    [x, y, z] =  tdoa2([field_data.mics.x; field_data.mics.y; field_data.mics.z ]', Hdist, Fs);
-    
-    % Append location to history
-    location = [location; [x y]];
+    %Initial path
+    location = start_location;
+    [x_nav, y_nav, ang_nav] = main(location(end,:), start_angle, final_location, [field_data.field.x_min,field_data.field.x_max,field_data.field.y_min,field_data.field.y_max]/100);
 
-    % Get current diameter from c++ routing
-    perimeter = [field_data.field.x_min field_data.field.x_max field_data.field.y_min field_data.field.y_max];
-    [x_nav, y_nav, ang_nav, success] = main(start_location, start_angle, final_location, perimeter);
-    radius_arr = 0.5 * r1 * tan(0.5 * (pi - ang_nav));
+    for idx = 1:(length(ang_nav)/2)
+      f(idx) = parfeval(p,@RecordLive,2,true,5,false); % Square size determined by idx
+    end
+    % Collect the results as they become available.
+    magicResults = cell(1,length(ang_nav));
+    KITT.setMotorSpeed(26);
+    for idx = 1:(length(ang_nav)/2)
+      tic;
+      % fetchNext blocks until next results are available.
+      [completedIdx,Hdist,Fs] = fetchNext(f,0.1);
+      % When new location is available plot it
+      if Hdist
+          [x, y, z] =  tdoa2([field_data.mics.x; field_data.mics.y; field_data.mics.z ]', Hdist, Fs);
+          location = [location; x*100, y*100];
+          hold on; p = plot(x, y, '.');
+          p.LineWidth = 2;
+          p.MarkerSize = 14;
+          p.MarkerFaceColor = 'white'; 
+          hold off;
+          fprintf('Got result with index: %d.\n', completedIdx);
+      end
+      clear Hdist;
+      
+      % Filter out infinite measurements
+       radius_arr = 0.5 * r1 * tan(0.5 * (pi - ang_nav));
     % Filter out infinite measurements
     radius_arr(radius_arr > 1E6) = 0;
     Diameter = 2*radius_arr(1);
-
-    % Use diameter to set direction
-    dir = Diameter2SteerDirection(Diameter);
-    KITT.setSteerDirection(dir);
-
-    % Eventueel snelheid gaan meten en variabel maken, zodat de auto zowel in
-    % de bochten als rechtdoor even snel gaat
-    KITT.setMotorSpeed(26);
-
-    % If within 15 cm of final location, break
-    if abs(final_location - location(end)) < 0.15
-        KITT.setMotorSpeed(15);
+      
+      % Set the steering direction
+      [dia,t] = Diameter2SteerDirection(Diameter);
+      while (toc < 2*t)
+         KITT.setSteerDirection(dia);
+      end
+    end
+    
+KITT.setMotorSpeed(15);
+    % If within 30 cm of final location, break
+    if abs(final_location - location(end,:)) < 0.3
+        KITT.setMotorSpeed(0);pause(0.3);KITT.setMotorSpeed(15);
+        fprintf('YEAH Arrived at the destination');
         break; 
+    else
+        fprintf('Almost there I have to retry');
     end
 end
--- Diameter2SteerDirection.m
+++ Diameter2SteerDirection.m
@@ -1,14 +1,15 @@
-function SteerDirection = Diameter2SteerDirection(Diameter)
+function [SteerDirection,Time] = Diameter2SteerDirection(Diameter)
     % When the diameter is 0, just go straight. Infinity is not a very nice number
     if (Diameter == 0)
-        SteerDirection = 0;
-
+        SteerDirection = 5;
+        Time = 0.109;
     % Negative diameter means steering left
     elseif (Diameter > 0)
         SteerDirection = -1.806e+10*exp(-15*Diameter) + -70.74*exp(-0.481*Diameter);
-
+        Time = 0.11;
     % Positive means steering right
     else
-        SteerDirection = 288.6*exp(1.684*Diameter) + 35.29*exp(0.1007*Diameter);
+        SteerDirection = 288.6*exp(1.684*Diameter) + 35.29*exp(0.1007*Diameter) +2.1;
+        Time = 0.11;
     end
 end
