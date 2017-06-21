1509911d8dd634c1a2f0286bb7c61024764e2db2
NielsFakkel <n.e.fakkel@student.tudelft.nl>
2017-06-21 23:46:33 +0200

    Dit word m de steering control

--- Diameter2SteerDirection.m
+++ Diameter2SteerDirection.m
@@ -1,20 +1,20 @@
-function [SteerDirection,Time] = Diameter2SteerDirection(Diameter, Index)
+function [SteerDirection,Time] = Diameter2SteerDirection(Diameter, Twindex)
     % When the diameter is 0, just go straight. Infinity is not a very nice number
+    if Twindex < 10
+            Time = 0.6 - 0.19*log(x);
+        else
+            Time = 0.15;
+    end
     if (Diameter == 0)
         SteerDirection = 6;
-        Index = Index * 0.5;
-        if Index < 20
-            Time = 0.4725 *exp(-0.8086 * Index) + 0.1491*exp(-0.03418 * Index);
-        else
-            Time = 0.075;
-        end
+                
     % Negative diameter means steering right
     elseif (Diameter > 0)
         SteerDirection = -1.806e+10*exp(-15*Diameter) + -70.74*exp(-0.481*Diameter);
-        Time = 0.109;
+%         Time = 0.109;
     % Positive means steering left
     else
         SteerDirection = 288.6*exp(1.684*Diameter) + 35.29*exp(0.1007*Diameter) +2.1;
-        Time = 0.117;
+%         Time = 0.117;
     end
 end
